87a798edd3c8030ba78886804049b527f5c6c4ec
nohdongyeon <ehddus20201@naver.com>
2022-11-04 15:48:33 +0900

    broom, house, camera, moving stone

---
+++ Assets/RigidbodyMove.cs
@@ -0,0 +1,38 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyMove : MonoBehaviour
+{
+    Rigidbody rb;
+    float power = 30f;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            rb.AddForce(Vector3.left * power);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            rb.AddForce(Vector3.right * power);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            rb.AddForce(Vector3.forward * power);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            rb.AddForce(Vector3.back * power);
+        }
+    }
+}
