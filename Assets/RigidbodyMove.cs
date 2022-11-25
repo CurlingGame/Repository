704dd92f06af1f9ca3d317534268db2bbdc74a70
노준호 <a2372557@naver.com>
2022-11-26 06:50:29 +0900

    11월4주차
    카메라 조정
    시트 마찰력 추가 (자연스러움)
    start라인에서 hoge 라인까지
    addforce하는 코드 작성
    + 하우스 내부로 넣기가 너무 힘들다
       조정필요

--- Assets/RigidbodyMove.cs
+++ Assets/RigidbodyMove.cs
@@ -1,38 +1,70 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RigidbodyMove : MonoBehaviour
 {
+    float power = 2.0f;
     Rigidbody rb;
-    float power = 200f;
 
-    void Start()
-    {
+    void Start() {
         rb = gameObject.GetComponent<Rigidbody>();
     }
 
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.LeftArrow) == true)
-        {
-            rb.AddForce(Vector3.left * power);
-        }
+    void Update() {
+        Vector3 pos;
+        pos = transform.position; // 현제 오브젝트 위치
 
-        if (Input.GetKey(KeyCode.RightArrow) == true)
-        {
-            rb.AddForce(Vector3.right * power);
-        }
-
-        if (Input.GetKey(KeyCode.UpArrow) == true)
-        {
-            rb.AddForce(Vector3.forward * power);
-        }
-
-        if (Input.GetKey(KeyCode.DownArrow) == true)
-        {
-            rb.AddForce(Vector3.back * power);
+        if (pos.x < -11.0f){
+            if (Input.GetKey(KeyCode.Space)){
+                if ((-18.0f < pos.x)&&(power == 2.0f)){
+                    power = power + 3.0f;
+                    Debug.Log(power);
+                }
+                if ((-15.0f < pos.x))
+                {
+                    Debug.Log(power);
+                }
+                if ((-13.0f < pos.x)&&(power == 5.0f)){
+                    power = power + 5.0f;
+                    Debug.Log(power);
+                }
+                rb.AddForce(Vector3.right * power);
+                /*Debug.Log("스페이스 키 누르는 중");*/
+            }
         }
     }
+
+    void LastUpdate() { }
 }
+
+/*Rigidbody rb;
+float power = 200f;
+
+void Start()
+{
+    rb = gameObject.GetComponent<Rigidbody>();
+}
+
+void Update()
+{
+    if (Input.GetKey(KeyCode.LeftArrow) == true)
+    {
+        rb.AddForce(Vector3.left * power);
+    }
+
+    if (Input.GetKey(KeyCode.RightArrow) == true)
+    {
+        rb.AddForce(Vector3.right * power);
+    }
+
+    if (Input.GetKey(KeyCode.UpArrow) == true)
+    {
+        rb.AddForce(Vector3.forward * power);
+    }
+
+    if (Input.GetKey(KeyCode.DownArrow) == true)
+    {
+        rb.AddForce(Vector3.back * power);
+    }
+}*/
