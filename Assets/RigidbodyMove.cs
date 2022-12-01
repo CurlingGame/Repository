10e0785e02b6b580bc15e265e0cb40ff45b58d20
노준호 <a2372557@naver.com>
2022-12-01 16:23:26 +0900

    Update RigidbodyMove.cs

--- Assets/RigidbodyMove.cs
+++ Assets/RigidbodyMove.cs
@@ -37,3 +37,34 @@ public class RigidbodyMove : MonoBehaviour
 
     void LastUpdate() { }
 }
+
+/*Rigidbody rb;
+float power = 200f;
+
+void Start()
+{
+    rb = gameObject.GetComponent<Rigidbody>();
+}
+
+void Update()
+{
+    if (Input.GetKey(KeyCode.LeftArrow) == true)
+    {
+        rb.AddForce(Vector3.left * power);
+    }
+
+    if (Input.GetKey(KeyCode.RightArrow) == true)
+    {
+        rb.AddForce(Vector3.right * power);
+    }
+
+    if (Input.GetKey(KeyCode.UpArrow) == true)
+    {
+        rb.AddForce(Vector3.forward * power);
+    }
+
+    if (Input.GetKey(KeyCode.DownArrow) == true)
+    {
+        rb.AddForce(Vector3.back * power);
+    }
+}*/
