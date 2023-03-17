29292512924b588badf6f9f463caa11331f962e9
nohdongyeon <ehddus20201@naver.com>
2023-03-17 18:32:25 +0900

    UI Canvas panel 및 버튼구현
    카메라 변경 코드작성, 하우스카메라 만들기, 아이콘 넣기, 음향문제 해결해야함.

---
+++ Assets/GameUI.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+    public GameObject InGameUIPanel;
+    public GameObject InGameSettingPanel;
+  
+
+    public void OnClickCamChange()
+    {   //카메라 전환 버튼을 눌렀을때 하우스를 보고 있는 카메라로 카메라 전환
+
+    }
+
+    public void OnClickStart()
+    {   //스타트 버튼을 누르기 전에는 마우스 휠이 작동되지 않아야함.
+        InGameUIPanel.SetActive(false);
+    }
+
+    public void OnClickInGametoSetting()
+    {
+        InGameSettingPanel.SetActive(true);
+        InGameUIPanel.SetActive(false);
+
+    }
+
+    public void OnClickSettingtoInGame()
+    {
+        InGameSettingPanel.SetActive(false);
+        InGameUIPanel.SetActive(true);
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
