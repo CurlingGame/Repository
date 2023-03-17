0a6733af5cd3d3a38441f96d5489bd61441ca882
nohdongyeon <ehddus20201@naver.com>
2023-03-17 18:33:42 +0900

    코드에 하우스캠 오브젝트 추가, 인스펙터엔 추가 X

--- Assets/GameUI.cs
+++ Assets/GameUI.cs
@@ -7,7 +7,8 @@ public class GameUI : MonoBehaviour
 {
     public GameObject InGameUIPanel;
     public GameObject InGameSettingPanel;
-  
+    public GameObject HouseCam; //카메라 전환 버튼을 눌렀을때 하우스의 상황을 보여줄 카메라
+
 
     public void OnClickCamChange()
     {   //카메라 전환 버튼을 눌렀을때 하우스를 보고 있는 카메라로 카메라 전환
